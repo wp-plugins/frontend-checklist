3f73db5d5f12b4af7516c41c3e589bbe424e9b1f
JonasBreuer <JonasBreuer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-01 15:23:28 +0000

    First release, HTML and PDF Checklist, with our without cookies
    git-svn-id: https://plugins.svn.wordpress.org/frontend-checklist/trunk@593415 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ frontend-checklist.js
@@ -0,0 +1,15 @@
+function frontend_checklist_save_cookie() {
+	var i;
+	var cookie_sum = 0;
+	var checkbox;
+	
+	for (i=0;i<=20;i++) {
+		checkbox = document.getElementById('frontend-checklist-todo-'+i);
+		if (checkbox && checkbox.checked) {
+			cookie_sum += Math.pow(2, i);
+		}
+	}
+	
+	document.cookie = 'frontend_checklist='+cookie_sum+"; expires=Tue, 07 Nov 2084 12:00:00 GMT";
+
+}
